cd375f72b777f068710bfe29cd61e45db44f80f0
Matt Pocock <mattpocockvoice@gmail.com>
2025-01-29 11:28:57 +0000

    Added a serper tool

--- examples/tools/01-serper/main.ts
+++ examples/tools/01-serper/main.ts
@@ -2,23 +2,28 @@ import { streamText } from "ai";
 import { flagshipAnthropicModel } from "../../_shared/models.ts";
 import { serper } from "./tools.ts";
 import dedent from "dedent";
+import { cliChat } from "../../_shared/cli-chat.ts";
 
-const result = streamText({
-  model: flagshipAnthropicModel,
-  tools: {
-    ...serper.tools,
+await cliChat({
+  intro: `Welcome to the Serper tool!`,
+  answerQuestion: async (messages) => {
+    const result = streamText({
+      model: flagshipAnthropicModel,
+      tools: {
+        ...serper.tools,
+      },
+      system: dedent`
+        You are a research agent. You can search the web to
+        find out information.
+    
+        For every fact you return, reference the URL you used
+        to find out the information.
+      `,
+      messages,
+      maxSteps: 5,
+    });
+
+    return result;
   },
-  system: dedent`
-    You are a research agent. You can search the web to
-    find out information.
-
-    For every fact you return, reference the URL you used
-    to find out the information.
-  `,
-  prompt: `Tell me about the history of the antelope.`,
-  maxSteps: 5,
+  dollarsPerMillionTokens: 3,
 });
-
-for await (const chunk of result.textStream) {
-  process.stdout.write(chunk);
-}
