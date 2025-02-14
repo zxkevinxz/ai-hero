bf0aff19880a139c6321c05861551d20409b94bf
Matt Pocock <mattpocockvoice@gmail.com>
2025-02-14 08:50:26 +0000

    Added a message annotations demo

---
+++ examples/ui-patterns/07-message-annotations-TODO/server/server.ts
@@ -0,0 +1,36 @@
+import {
+  createDataStreamResponse,
+  streamText,
+  type Message,
+} from "ai";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { flagshipAnthropicModel } from "../../../_shared/models.ts";
+
+export const honoApp = new Hono();
+
+honoApp.use(cors());
+
+honoApp.post("/api/chat", async (ctx) => {
+  const { messages }: { messages: Message[] } =
+    await ctx.req.json();
+
+  return createDataStreamResponse({
+    execute: async (dataStream) => {
+      const interval = setInterval(() => {
+        dataStream.writeMessageAnnotation({
+          eyo: true,
+        });
+      }, 100);
+      const result = streamText({
+        model: flagshipAnthropicModel,
+        messages,
+        onFinish: () => {
+          clearInterval(interval);
+        },
+      });
+
+      result.mergeIntoDataStream(dataStream);
+    },
+  });
+});
