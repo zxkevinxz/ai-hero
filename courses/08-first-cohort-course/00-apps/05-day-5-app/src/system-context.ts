17c611e4598855f7b08b1690d0e3116102bc52a7
Matt Pocock <mattpocockvoice@gmail.com>
2025-04-25 13:48:31 +0100

    Added system context file

---
+++ courses/08-first-cohort-course/00-apps/05-day-5-app/src/system-context.ts
@@ -0,0 +1,72 @@
+type QueryResultSearchResult = {
+  date: string;
+  title: string;
+  url: string;
+  snippet: string;
+};
+
+type QueryResult = {
+  query: string;
+  results: QueryResultSearchResult[];
+};
+
+type ScrapeResult = {
+  url: string;
+  result: string;
+};
+
+const toQueryResult = (query: QueryResultSearchResult) =>
+  [`### ${query.date} - ${query.title}`, query.url, query.snippet].join("\n\n");
+
+export class SystemContext {
+  /**
+   * The current step in the loop
+   */
+  private step = 0;
+
+  /**
+   * The history of all queries searched
+   */
+  private queryHistory: QueryResult[] = [];
+
+  /**
+   * The history of all URLs scraped
+   */
+  private scrapeHistory: ScrapeResult[] = [];
+
+  shouldStop() {
+    return this.step >= 10;
+  }
+
+  reportQueries(queries: QueryResult[]) {
+    this.queryHistory.push(...queries);
+  }
+
+  reportScrapes(scrapes: ScrapeResult[]) {
+    this.scrapeHistory.push(...scrapes);
+  }
+
+  getQueryHistory(): string {
+    return this.queryHistory
+      .map((query) =>
+        [
+          `## Query: "${query.query}"`,
+          ...query.results.map(toQueryResult),
+        ].join("\n\n"),
+      )
+      .join("\n\n");
+  }
+
+  getScrapeHistory(): string {
+    return this.scrapeHistory
+      .map((scrape) =>
+        [
+          `## Scrape: "${scrape.url}"`,
+          `<scrape_result>`,
+          scrape.result,
+          `</scrape_result>`,
+        ].join("\n\n"),
+      )
+      .join("\n\n");
+  }
+}
